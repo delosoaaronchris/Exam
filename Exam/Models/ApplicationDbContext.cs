f6e940b588f961a39fc83f96bf8adf58bb1c2f59
Aaron Chris <Aaron Chris@DESKTOP-K35K5J0>
2024-06-01 03:52:41 +0800

    Add project files.

---
+++ Exam/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+
+namespace RecyclingApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<RecyclableType> RecyclableTypes { get; set; }
+        public DbSet<RecyclableItem> RecyclableItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecyclableType>()
+                .HasIndex(rt => rt.Type)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Exam/Models/RecyclableItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecyclingApp.Models
+{
+    public class RecyclableItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("RecyclableType")]
+        public int RecyclableTypeId { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
+        public decimal Weight { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
+        public decimal ComputedRate { get; set; }
+
+        [StringLength(150)]
+        public string ItemDescription { get; set; }
+
+        public virtual RecyclableType RecyclableType { get; set; }
+
+        // Method to compute ComputedRate based on RecyclableType rate and Weight
+        public void ComputeRate(decimal recyclableTypeRate)
+        {
+            // Compute ComputedRate
+            ComputedRate = Math.Round(recyclableTypeRate * Weight, 2);
+        }
+    }
+}
